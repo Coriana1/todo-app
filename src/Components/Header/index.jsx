849f5f73b1c89bff6189e19e981c8b697705a2b2
Coriana Williams <corianacory@gmail.com>
2023-07-09 13:06:17 -0400

    worked on lab 33

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,31 +1,50 @@
-import { createStyles, Header } from "@mantine/core";
-import { Link } from "react-router-dom";
+import { useContext, useState } from 'react';
+import { SettingsContext } from '../../Context/Settings';
+import { Pagination, Card, Badge, CloseButton} from '@mantine/core';
+import { When } from 'react-if';
+import Auth from '../Auth';
 
-const useStyles = createStyles((theme) => ({
-    navbar: {
-    backgroundColor: theme.colors.blue[6],
-    height: '100%',
-    margin: '0',
-    color: theme.colors.gray[0],
-    display: 'flex',
-    padding: theme.spacing.sm,
-    fontSize: theme.fontSizes.md,
-    },
-    link: {
-        padding: theme.spacing.sm,
-    }
-}));
+function List({list, toggleComplete, deleteItem}){
+    const { pageItems, showCompleted } = useContext(SettingsContext);
+    const [currentPage, setPage] = useState(1)
+    
+    const displayedItems = showCompleted
+    ? list
+    : list.filter((item) => !item.complete);
+    
+    const pages = Math.ceil(displayedItems.length / pageItems)
+    const firstItem = (currentPage - 1) * pageItems;
+    const lastItem = currentPage * pageItems;
+    const finalItems = displayedItems.slice(firstItem, lastItem);
 
-function HeaderComponent(){
-    const { classes } = useStyles();
+
     return(
-        <Header data-testid="header">
-            <nav className={classes.navbar} >
-                <Link className={classes.link} to='/'>Home</Link>
-                <Link className={classes.link} to='/settings'>Settings</Link>
-            </nav>
-        </Header>
+      <>
+        {finalItems.map(item => (
+        <Card shadow="sm" padding="lg" radius="md" withBorder key={item.id} >
+          <When condition={!item.complete}>
+            <Badge color="green" variant="filled" onClick={() => toggleComplete(item.id)}>Pending</Badge> {item.assignee}    
+            <Auth capability={'delete'}>
+              <CloseButton onClick={() => deleteItem(item.id)}/>
+            </Auth>
+            <hr />
+            <p>{item.text}</p>
+            <p><small>Difficulty: {item.difficulty}</small></p>
+          </When>
+          <When condition={item.complete}>
+            <Badge color="red" variant="filled" onClick={() => toggleComplete(item.id)}>Completed</Badge> {item.assignee}
+            <Auth capability={'delete'}>
+              <CloseButton onClick={() => deleteItem(item.id)}/>
+            </Auth>
+            <hr />
+            <p>{item.text}</p>
+            <p><small>Difficulty: {item.difficulty}</small></p>
+          </When>
+        </Card>
+      ))}
+      <Pagination value={currentPage} onChange={setPage} total={pages} />
+      </>
     )
 }
 
-export default HeaderComponent;
+export default List;
--- src/Components/List/index.jsx
+++ src/Components/List/index.jsx
@@ -1,112 +1,74 @@
-import React, { useEffect, useState } from 'react';
-import useForm from '../../hooks/form';
-import List from '../List';
-import { Button, TextInput, Grid, Slider, Card, createStyles } from '@mantine/core';
-import { v4 as uuid } from 'uuid';
+import { useContext, useState } from 'react';
+import { SettingsContext } from '../../Context/Settings';
+import { Card, Badge, CloseButton } from '@mantine/core';
+import { When } from 'react-if';
+import Auth from '../Auth';
+import { Pagination } from '@mantine/core';
 
-const useStyles = createStyles((theme) => ({
-  h1: {
-    backgroundColor: theme.colors.gray[8],
-    color: 'white',
-    fontSize: '20px',
-    fontWeight: 'bold',
-    margin: '16px auto',
-    padding: '16px',
-    width: '80%',
-    fontFamily: 'apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif',
-  }
-}));
+function List({ list, toggleComplete, deleteItem }) {
+  const { pageItems, showCompleted } = useContext(SettingsContext);
+  const [currentPage, setPage] = useState(1);
 
-const Todo = () => {
-
-  const { classes } = useStyles();
-  const [defaultValues] = useState({
-    difficulty: 3,
-  });
-  const [list, setList] = useState([]);
-  const [incomplete, setIncomplete] = useState([]);
-  const { handleChange, handleSubmit } = useForm(addItem, defaultValues);
-
-  function addItem(item) {
-    item.id = uuid();
-    item.complete = false;
-    console.log(item);
-    setList([...list, item]);
+  // Check if the list is defined and not empty
+  if (!list || list.length === 0) {
+    return null; // Or display a message indicating no items
   }
 
-  function deleteItem(id) {
-    const items = list.filter( item => item.id !== id );
-    setList(items);
-  }
+  const displayedItems = showCompleted ? list : list.filter((item) => !item.complete);
 
-  function toggleComplete(id) {
+  const pages = Math.ceil(displayedItems.length / pageItems);
+  const firstItem = (currentPage - 1) * pageItems;
+  const lastItem = currentPage * pageItems;
+  const finalItems = displayedItems.slice(firstItem, lastItem);
 
-    const items = list.map( item => {
-      if ( item.id === id ) {
-        item.complete = ! item.complete;
-      }
-      return item;
-    });
-
-    setList(items);
-
-  }
-
-  useEffect(() => {
-    let incompleteCount = list.filter(item => !item.complete).length;
-    setIncomplete(incompleteCount);
-    document.title = `To Do List: ${incomplete}`;
-    // linter will want 'incomplete' added to dependency array unnecessarily. 
-    // disable code used to avoid linter warning 
-    // eslint-disable-next-line react-hooks/exhaustive-deps 
-  }, [list]);  
+  const handlePageChange = (selectedPage) => {
+    setPage(selectedPage.selected + 1);
+  };
 
   return (
     <>
-      <h1 data-testid="header-h1" className={classes.h1}>To Do List: {incomplete} items pending</h1>
-      {/* leave the form code inside of the Todo Component */}
-      <Grid style={{width: '80%', margin: 'auto'}}>
-        <Grid.Col span={6}>
-          <Card shadow="sm" padding="lg" radius="md" withBorder>
-          <form onSubmit={handleSubmit}>
-            <h2>Add To Do Item</h2>
-
-            <label>
-              <span>To Do Item</span>
-              <TextInput onChange={handleChange} name="text" type="text" placeholder="Item Details" />
-            </label>
-
-            <label>
-              <span>Assigned To</span>
-              <TextInput onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
-            </label>
-            <label>
-              <span>Difficulty</span>
-              <Slider
-                    marks={[
-                      { value: 20 },
-                      { value: 40 },
-                      { value: 60 },
-                      { value: 80 },
-                      { value: 100 },
-                    ]}
-                    onChange={handleChange} defaultValue={defaultValues.difficulty} type="range" min={1} max={5} name="difficulty"/>
-            </label>
-
-            <label>
-              <Button type="submit">Add Item</Button>
-            </label>
-          </form>
-          </Card>
-        </Grid.Col>
-        <Grid.Col span={6}>
-          <Card shadow="sm" padding="lg" radius="md" withBorder >
-            <List list={list} toggleComplete={toggleComplete} deleteItem={deleteItem} />
-          </Card>
-        </Grid.Col>
-        </Grid>
+      {finalItems.map((item) => (
+        <Card shadow="sm" padding="lg" radius="md" withBorder key={item.id}>
+          <When condition={!item.complete}>
+            <Badge color="green" variant="filled" onClick={() => toggleComplete(item.id)}>
+              Pending
+            </Badge>{' '}
+            {item.assignee}
+            <Auth capability={'delete'}>
+              <CloseButton onClick={() => deleteItem(item.id)} />
+            </Auth>
+            <hr />
+            <p>{item.text}</p>
+            <p>
+              <small>Difficulty: {item.difficulty}</small>
+            </p>
+          </When>
+          <When condition={item.complete}>
+            <Badge color="red" variant="filled" onClick={() => toggleComplete(item.id)}>
+              Completed
+            </Badge>{' '}
+            {item.assignee}
+            <Auth capability={'delete'}>
+              <CloseButton onClick={() => deleteItem(item.id)} />
+            </Auth>
+            <hr />
+            <p>{item.text}</p>
+            <p>
+              <small>Difficulty: {item.difficulty}</small>
+            </p>
+          </When>
+        </Card>
+      ))}
+      <Pagination
+        value={currentPage}
+        onChange={handlePageChange}
+        total={pages}
+        limit={5} // Add a limit for pagination items
+      />
     </>
   );
-};
+}
 
-export default Todo;
+export default List;
+
+
